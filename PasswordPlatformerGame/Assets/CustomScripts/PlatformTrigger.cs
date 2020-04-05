e3b31d04f9b087cbb8f860a4e811f9b23135cfe3
Taylor Smith <tas12740@gmail.com>
2020-04-04 22:12:58 -0400

    Unity build process worksgit add passwordplatformer/git add passwordplatformer/!

---
+++ PasswordPlatformerGame/Assets/CustomScripts/PlatformTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    private bool hasCollided = false;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!this.hasCollided)
+        {
+            PlatformGenerator.Instance.generatePlatforms();
+        }
+    }
+}
