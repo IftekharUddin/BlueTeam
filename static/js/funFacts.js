0677a39a0383e3805fa793fc1660f69805406188
Taylor Smith <tas12740@gmail.com>
2020-04-04 22:12:21 -0400

    Allow JS import to work on Chrome by adding export to funFacts file and adding type=module to index file

--- static/js/funFacts.js
+++ static/js/funFacts.js
@@ -1,7 +1,11 @@
 export let funFacts = [
-    "In 2018 hackers stole half a billion personal records", 
-    "There is a hacker attack every 39 seconds", 
-    "66% of people use only 1 or 2 passwords for all their accounts", 
-    "48% of people have shared a password with someone else", 
+    "In 2018 hackers stole half a billion personal records",
+    "There is a hacker attack every 39 seconds",
+    "66% of people use only 1 or 2 passwords for all their accounts",
+    "48% of people have shared a password with someone else",
     "'123456' is the most commonly used password",
 ];
+
+export default {
+    funFacts
+};
