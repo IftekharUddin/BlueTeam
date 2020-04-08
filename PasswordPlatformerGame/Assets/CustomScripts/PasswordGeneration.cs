d779a841c9b2da774f72cee4020b33123e612213
Taylor Smith <tas12740@gmail.com>
2020-04-07 20:44:41 -0400

    Adding a start screen which can load the playing screen with different difficulty levels

--- PasswordPlatformerGame/Assets/CustomScripts/PasswordGeneration.cs
+++ PasswordPlatformerGame/Assets/CustomScripts/PasswordGeneration.cs
@@ -7,6 +7,13 @@ using System.Text;
 
 public class PasswordGeneration : MonoBehaviour
 {
+    private enum Difficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD
+    }
+    private Difficulty difficulty;
     public TextAsset badPasswordsText;
     public TextAsset wordsText;
 
@@ -75,6 +82,26 @@ public class PasswordGeneration : MonoBehaviour
         }
     }
 
+    void OnEnable()
+    {
+        int difficulty = PlayerPrefs.GetInt("difficulty");
+        switch (difficulty)
+        {
+            case 0:
+                this.difficulty = Difficulty.EASY;
+                break;
+            case 1:
+                this.difficulty = Difficulty.MEDIUM;
+                break;
+            case 2:
+                this.difficulty = Difficulty.HARD;
+                break;
+            default:
+                Application.Quit();
+                return;
+        }
+    }
+
     public string EvaluatePassword(string password)
     {
         var result = this.passwordChecker.EvaluatePassword(password);
@@ -103,116 +130,57 @@ public class PasswordGeneration : MonoBehaviour
         //         ['t'] = "+7",
         //         ['x'] = "%",
         //         ['z'] = "2"
-        if (ch == 'a' || ch == 'A')
+        switch (ch)
         {
-            return sub(ch, new string[] { "4", "@" });
+            case 'a':
+            case 'A':
+                return sub(ch, new string[] { "4", "@" });
+            case 'b':
+            case 'B':
+                return sub(ch, new string[] { "8" });
+            case 'c':
+            case 'C':
+                return sub(ch, new string[] { "(", "{", "[", "<" });
+            case 'e':
+            case 'E':
+                return sub(ch, new string[] { "3" });
+            case 'g':
+            case 'G':
+                return sub(ch, new string[] { "6", "9" });
+            case 'i':
+            case 'I':
+                return sub(ch, new string[] { "1", "!", "|" });
+            case 'l':
+            case 'L':
+                return sub(ch, new string[] { "1", "|", "7" });
+            case 'o':
+            case 'O':
+                return sub(ch, new string[] { "0" });
+            case 's':
+            case 'S':
+                return sub(ch, new string[] { "$", "5" });
+            case 't':
+            case 'T':
+                return sub(ch, new string[] { "+", "7" });
+            case 'x':
+            case 'X':
+                return sub(ch, new string[] { "%" });
+            case 'z':
+            case 'Z':
+                return sub(ch, new string[] { "2" });
+            default:
+                return "" + ch;
         }
-        else if (ch == 'b' || ch == 'B')
-        {
-            return sub(ch, new string[] { "8", "|3", "|8", "lo", "|o", });
-        }
-        else if (ch == 'c' || ch == 'C')
-        {
-            return sub(ch, new string[] { "<", "{", "[", "(" });
-        }
-        else if (ch == 'd' || ch == 'D')
-        {
-            return sub(ch, new string[] { "|)", "|}", "|]", "|>" });
-        }
-        else if (ch == 'e' || ch == 'E')
-        {
-            return sub(ch, new string[] { "3" });
-        }
-        else if (ch == 'f' || ch == 'F')
-        {
-            return "" + ch;
-        }
-        else if (ch == 'g' || ch == 'G')
-        {
-            return sub(ch, new string[] { "6" });
-        }
-        else if (ch == 'h' || ch == 'H')
-        {
-            return sub(ch, new string[] { "|-|", "[-]", "}-{", "}{", "|=|", "[=]" });
-        }
-        else if (ch == 'i' || ch == 'I')
-        {
-            return sub(ch, new string[] { "1", "|", "!" });
-        }
-        else if (ch == 'j' || ch == 'J')
-        {
-            return sub(ch, new string[] { "_|", "_)", "_]", "_}" });
-        }
-        else if (ch == 'k' || ch == 'K')
-        {
-            return sub(ch, new string[] { "|<", "1<", "l<" });
-        }
-        else if (ch == 'l' || ch == 'L')
-        {
-            return sub(ch, new string[] { "|_", "|", "1" });
-        }
-        else if (ch == 'm' || ch == 'M')
-        {
-            return sub(ch, new string[] { "|\\/|", "/\\/\\" });
-        }
-        else if (ch == 'n' || ch == 'N')
-        {
-            return sub(ch, new string[] { "|\\|" });
-        }
-        else if (ch == 'o' || ch == 'O')
-        {
-            return sub(ch, new string[] { "0", "()", "[]", "{}", "<>", "oh" });
-        }
-        else if (ch == 'p' || ch == 'P')
-        {
-            return sub(ch, new string[] { "|O", "|>", "|*" });
-        }
-        else if (ch == 'q' || ch == 'Q')
-        {
-            return sub(ch, new string[] { "O_" });
-        }
-        else if (ch == 'r' || ch == 'R')
-        {
-            return "" + ch;
-        }
-        else if (ch == 's' || ch == 'S')
-        {
-            return sub(ch, new string[] { "5", "$" });
-        }
-        else if (ch == 't' || ch == 'T')
-        {
-            return sub(ch, new string[] { "+", "'|'", "`|`", "~|~", "-|-" });
-        }
-        else if (ch == 'u' || ch == 'U')
-        {
-            return sub(ch, new string[] { "|_|" });
-        }
-        else if (ch == 'v' || ch == 'V')
-        {
-            return sub(ch, new string[] { "\\/" });
-        }
-        else if (ch == 'w' || ch == 'W')
-        {
-            return sub(ch, new string[] { "\\/\\/" });
-        }
-        else if (ch == 'x' || ch == 'X')
-        {
-            return sub(ch, new string[] { "><" });
-        }
-        else if (ch == 'y' || ch == 'Y')
-        {
-            return "" + ch;
-        }
-        else if (ch == 'z' || ch == 'Z')
-        {
-            return "" + ch;
-        }
-        return "" + ch;
     }
 
     private string sub(char ch, string[] choices)
     {
-        return (Random.value > .1f) ? ("" + ch) : choices[Mathf.FloorToInt(Random.Range(0, choices.Length))];
+        return sub(ch, choices, .1f);
+    }
+
+    private string sub(char ch, string[] choices, float probLeet)
+    {
+        return (Random.value > probLeet) ? ("" + ch) : choices[Mathf.FloorToInt(Random.value * choices.Length)];
     }
 
     private string l33tword(string word)
@@ -227,7 +195,7 @@ public class PasswordGeneration : MonoBehaviour
 
     private string getRandomWord()
     {
-        return this.commonWords[Mathf.FloorToInt(Random.Range(0, this.commonWords.Count))] as string;
+        return this.commonWords[Mathf.FloorToInt(Random.value * this.commonWords.Count)] as string;
     }
 
     private string generateGoodPassword()
@@ -244,8 +212,6 @@ public class PasswordGeneration : MonoBehaviour
     {
         string pw = generateGoodPassword();
 
-        // var result = this.passwordChecker.EvaluatePassword(first);
-        // Debug.Log($"{first}: {result.CrackTime} {result.CrackTimeDisplay} {result.Score} {result.CalcTime}");
         var result = this.passwordChecker.EvaluatePassword(pw);
 
         while (result.Score < 3)
---
+++ PasswordPlatformerGame/Assets/CustomScripts/startscreen/ButtonScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    private enum Difficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD
+    }
+    public Button easyButton, mediumButton, hardButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.easyButton.onClick.AddListener(Easy);
+        this.mediumButton.onClick.AddListener(Medium);
+        this.hardButton.onClick.AddListener(Hard);
+    }
+
+    void Easy()
+    {
+        loadPlayScene(Difficulty.EASY);
+    }
+
+    void Medium()
+    {
+        loadPlayScene(Difficulty.MEDIUM);
+    }
+
+    private void Hard()
+    {
+        loadPlayScene(Difficulty.HARD);
+    }
+
+    private void loadPlayScene(Difficulty chosen)
+    {
+        switch (chosen)
+        {
+            case Difficulty.EASY:
+                PlayerPrefs.SetInt("difficulty", 0);
+                break;
+            case Difficulty.MEDIUM:
+                PlayerPrefs.SetInt("difficulty", 1);
+                break;
+            case Difficulty.HARD:
+                PlayerPrefs.SetInt("difficulty", 2);
+                break;
+            default:
+                break;
+        }
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+}
