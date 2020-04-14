29094b49b60d143bccbabbee231b1a9568663a7d
Taylor Smith <tas12740@gmail.com>
2020-04-14 13:39:52 -0400

    Update game to have new communication with PHP on server and update JS to pull username from server

--- PasswordPlatformerGame/Assets/CustomScripts/Countdown.cs
+++ PasswordPlatformerGame/Assets/CustomScripts/Countdown.cs
@@ -8,7 +8,6 @@ public class Countdown : MonoBehaviour
     public Text timeText;
 
     // time limit set here in seconds
-//    float timeLeft = 45.0f;
     float timeLeft = 45.0f;
 
     // Update is called once per frame
